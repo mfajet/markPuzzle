e3d62e64c3420e39679fe96c3aa225d32645b0d3
Mark Fajet <markfajet@gmail.com>
2017-02-19 01:53:06 -0500

    functionality started

--- main.js
+++ main.js
@@ -2,6 +2,52 @@ var image = new Image();
 image.onload = cutImageUp;
 image.src = '/face7.PNG';
 
+document.onkeydown = checkKey;
+var emptyBox = 1;
+function checkKey(e) {
+
+    e = e || window.event;
+    if (e.keyCode == '38') {//up
+        if(emptyBox<7){
+            moveTo = emptyBox;
+            emptyBox = moveTo + 3;
+        }
+    }
+    else if (e.keyCode == '40') {
+        // down arrow
+        if(emptyBox>3){
+            moveTo = emptyBox;
+            emptyBox = moveTo - 3;
+        }
+    }
+    else if (e.keyCode == '37') {
+       // left arrow
+       if(emptyBox %3 !=0){
+           moveTo = emptyBox;
+           emptyBox = moveTo + 1;
+       }
+    }
+    else if (e.keyCode == '39') {
+       // right arrow
+       if(emptyBox %3 != 1){
+           moveTo = emptyBox;
+           emptyBox = moveTo -1;
+       }
+    }
+    if(moveTo != emptyBox){
+        console.log(moveTo);
+        console.log(emptyBox);
+        var from = document.getElementById(emptyBox);
+        var to = document.getElementById(moveTo);
+        var fromSrc = from.src;
+        from.src="";
+        to.src=fromSrc;
+        from.style.height = ""+heightOfOnePiece+"px";
+        from.style.width = ""+widthOfOnePiece+"px";
+    }
+}
+var heightOfOnePiece;
+var widthOfOnePiece;
 numColsToCut = 3;
 numRowsToCut = 3;
 function cutImageUp() {
@@ -35,7 +81,8 @@ function cutImageUp() {
         }
         used.push(r)
     }
-
+    var first = document.getElementById('1');
+    first.src="";
     for (var i = 2; i < 10; i++){
         var anImageElement = document.getElementById(i);
         anImageElement.src = imagePieces[used[i-1]];
