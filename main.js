ed62288b24740a55a843c5234aef6a8d0588b968
Mark Fajet <markfajet@gmail.com>
2017-02-19 02:18:44 -0500

    working game.

--- main.js
+++ main.js
@@ -1,36 +1,36 @@
 var image = new Image();
 image.onload = cutImageUp;
 image.src = '/face7.PNG';
+var white = new Image();
+white.onload = cropWhite;
+white.src ="/white.PNG";
 
 document.onkeydown = checkKey;
 var emptyBox = 1;
 function checkKey(e) {
-
+    console.log(emptyBox);
+    moveTo= emptyBox
     e = e || window.event;
     if (e.keyCode == '38') {//up
-        if(emptyBox<7){
-            moveTo = emptyBox;
+        if(emptyBox<7 && emptyBox){
             emptyBox = moveTo + 3;
         }
     }
     else if (e.keyCode == '40') {
         // down arrow
         if(emptyBox>3){
-            moveTo = emptyBox;
             emptyBox = moveTo - 3;
         }
     }
     else if (e.keyCode == '37') {
        // left arrow
        if(emptyBox %3 !=0){
-           moveTo = emptyBox;
            emptyBox = moveTo + 1;
        }
     }
     else if (e.keyCode == '39') {
        // right arrow
        if(emptyBox %3 != 1){
-           moveTo = emptyBox;
            emptyBox = moveTo -1;
        }
     }
@@ -40,23 +40,22 @@ function checkKey(e) {
         var from = document.getElementById(emptyBox);
         var to = document.getElementById(moveTo);
         var fromSrc = from.src;
-        from.src="";
+        from.src=whiteURL;
         to.src=fromSrc;
-        from.style.height = ""+heightOfOnePiece+"px";
-        from.style.width = ""+widthOfOnePiece+"px";
+        from.style.width = "100px";
     }
 }
 var heightOfOnePiece;
 var widthOfOnePiece;
 numColsToCut = 3;
 numRowsToCut = 3;
+var whiteURL;
 function cutImageUp() {
     widthOfOnePiece =this.width / 3;
     heightOfOnePiece=this.height / 3;
-    var emptyPic = document.getElementById('empty');
-    emptyPic.style.height = ""+heightOfOnePiece+"px";
-    emptyPic.style.width = ""+widthOfOnePiece+"px";
-    console.log(image);
+    var emptyPic = document.getElementById('1');
+    emptyPic.style.width = "100px";
+    emptyPic.style.height = "auto";
     var imagePieces = [];
         for(var y = 0; y < numRowsToCut; y++) {
             for(var x = 0; x < numColsToCut; x++) {
@@ -86,6 +85,24 @@ function cutImageUp() {
     for (var i = 2; i < 10; i++){
         var anImageElement = document.getElementById(i);
         anImageElement.src = imagePieces[used[i-1]];
+        anImageElement.style.width = "100px"
+        anImageElement.style.height= "auto";
     }
+
+}
+
+function cropWhite() {
+
+            var canvas = document.createElement('canvas');
+            canvas.width = widthOfOnePiece;
+            canvas.height = heightOfOnePiece;
+            var context = canvas.getContext('2d');
+            context.drawImage(white, widthOfOnePiece, heightOfOnePiece, widthOfOnePiece, heightOfOnePiece, 0, 0, canvas.width, canvas.height);
+            whiteURL = (canvas.toDataURL());
+            var emptyPic = document.getElementById('1');
+            emptyPic.src=whiteURL;
+            emptyPic.style.width = "100px";
+            emptyPic.style.height = "auto";
+
 
 }
