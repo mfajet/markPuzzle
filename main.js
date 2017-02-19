768e0f3429806a4fff1bbe6f8361164f6e14328b
Big Babby <Big Babby>
2017-02-19 00:35:51 -0500

    crop example

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-console.log("HI");
+var image = new Image();
+image.onload = cutImageUp;
+image.src = 'myimage.png';
+
+function cutImageUp() {
+    var imagePieces = [];
+    for(var x = 0; x < numColsToCut; ++x) {
+        for(var y = 0; y < numRowsToCut; ++y) {
+            var canvas = document.createElement('canvas');
+            canvas.width = widthOfOnePiece;
+            canvas.height = heightOfOnePiece;
+            var context = canvas.getContext('2d');
+            context.drawImage(image, x * widthOfOnePiece, y * heightOfOnePiece, widthOfOnePiece, heightOfOnePiece, 0, 0, canvas.width, canvas.height);
+            imagePieces.push(canvas.toDataURL());
+        }
+    }
+
+    // imagePieces now contains data urls of all the pieces of the image
+
+    // load one piece onto the page
+    var anImageElement = document.getElementById('myImageElementInTheDom');
+    anImageElement.src = imagePieces[0];
+}
